13ed2a4a3614c159e460736fa15079bba61987aa
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2025-02-03 07:53:30 +0000

    Trim down the resolve_reference.rb
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- tasks/resolve_reference.rb
+++ tasks/resolve_reference.rb
@@ -2,17 +2,45 @@
 # frozen_string_literal: true
 
 require 'json'
-require_relative '../lib/orbstack_bolt_inventory/inventory'
+require 'yaml'
 
-params = JSON.parse($stdin.read) # Read the input from Bolt
-group_patterns = params['group_patterns']
+# Load YAML from a file or string
+yaml_data = """
+---
+config:
+  transport: ssh
+  ssh:
+    native-ssh: true
+    load-config: true
+    login-shell: bash
+    tty: false
+    host-key-check: false
+    run-as: root
+    user: root
+    port: 32222
+targets:
+- name: agent01
+  uri: agent01@orb
+- name: agent02
+  uri: agent02@orb
+- name: agent03
+  uri: agent03@orb
+- name: compiler01
+  uri: compiler01@orb
+- name: compiler02
+  uri: compiler02@orb
+"""
 
-inventory = OrbstackBoltInventory::Inventory.new({ 'group_patterns' => group_patterns })
+# Convert YAML to a Ruby hash
+hash = YAML.load(yaml_data)
+result = { 'value' => hash }
+puts result.to_json
 
+exit 0
 # Generate the inventory and output the result
 begin
   # Wrap the inventory result in a 'value' key as expected by Bolt
-  result = { 'value' => inventory.generate }
+  result = { 'value' => hash }
   puts result.to_json
 rescue StandardError => e
   warn({ _error: { msg: e.message, kind: 'bolt/plugin-error' } }.to_json)
